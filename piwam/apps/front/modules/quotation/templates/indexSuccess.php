c18952a818e33fc65c8d01166303b77a32773e26
mercanj <mercan.j@gmail.com>
2012-01-18 20:00:31 +0100

    add Devis ink and page to add pdf generation

---
+++ piwam/apps/front/modules/quotation/templates/indexSuccess.php
@@ -0,0 +1,7 @@
+<!-- First tab : quotation creation -->
+
+<div>
+<h2>Creer un devis</h1>
+	<h3><a href="#">Generer un devis</a></h2>
+	<h3><a href="#">Exporter le devis au format pdf</a></h2>
+</div>
