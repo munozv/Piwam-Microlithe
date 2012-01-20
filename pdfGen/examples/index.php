0e05b5656b59f14eecbcef54e1f2b012d3caa887
munozv <vincent.munoz91@gmail.com>
2012-01-20 10:17:11 +0100

    pdfGenerator

---
+++ pdfGen/examples/index.php
@@ -0,0 +1,25 @@
+<?php
+echo '<'.'?'.'xml version="1.0" encoding="UTF-8"'.'?'.'>';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+
+<head>
+<title>PIWAM</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="description" content="TCPDF is a PHP class for generating PDF documents on the fly" />
+<meta name="author" content="Nicola Asuni" />
+<meta name="keywords" content="Examples, TCPDF, PDF, PHP class" />
+</head>
+
+<body>
+
+<h1>pdf piwam first release</h1>
+
+<ol>
+<li>Simple PDF with default Header and Footer: [<a href="pdf.php" title="PDF [new window]" target="_blank">PDF</a>]</li>
+</ol>
+
+</body>
+</html>
---
+++ pdfGen/examples/pdf.php
@@ -0,0 +1,100 @@
+<?php
+//============================================================+
+// File name   : example_001.php
+// Begin       : 2008-03-04
+// Last Update : 2010-08-14
+//
+// Description : Example 001 for TCPDF class
+//               Default Header and Footer
+//
+// Author: Nicola Asuni
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com LTD
+//               Manor Coach House, Church Hill
+//               Aldershot, Hants, GU12 4RQ
+//               UK
+//               www.tecnick.com
+//               info@tecnick.com
+//============================================================+
+
+/**
+ * Creates an example PDF TEST document using TCPDF
+ * @package com.tecnick.tcpdf
+ * @abstract TCPDF - Example: Default Header and Footer
+ * @author Nicola Asuni
+ * @since 2008-03-04
+ */
+
+require_once('../config/lang/eng.php');
+require_once('../tcpdf.php');
+
+// create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('cedric anceau');
+$pdf->SetTitle('devis microlhite');
+$pdf->SetSubject('piwam');
+$pdf->SetKeywords('piwam, epitech, pdf, pfa, soutenance intermediaire');
+
+// set default header data
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
+
+// set header and footer fonts
+$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+//set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+//set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+//set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+//set some language-dependent strings
+$pdf->setLanguageArray($l);
+
+// ---------------------------------------------------------
+
+// set default font subsetting mode
+$pdf->setFontSubsetting(true);
+
+// Set font
+// dejavusans is a UTF-8 Unicode font, if you only need to
+// print standard ASCII chars, you can use core fonts like
+// helvetica or times to reduce file size.
+$pdf->SetFont('dejavusans', '', 14, '', true);
+
+// Add a page
+// This method has several options, check the source code documentation for more information.
+$pdf->AddPage();
+
+// Set some content to print
+$html = <<<EOD
+  Ceci est un devis de notre part concernant votre implication dans notre entreprise, Veuillez racker svp, et nous remplir les poches.
+  Si vous etes un gros debile et que vous ne pouvez pas apprendre par vous meme, venez visite notre entreprise au  rue des triso dans le triangle de bermude.
+Soyez pas timide nous faisons de prix de groupe pour les autistes.
+EOD;
+
+// Print text using writeHTMLCell()
+$pdf->writeHTMLCell($w=0, $h=0, $x='', $y='', $html, $border=0, $ln=1, $fill=0, $reseth=true, $align='', $autopadding=true);
+
+// ---------------------------------------------------------
+
+// Close and output PDF document
+// This method has several options, check the source code documentation for more information.
+$pdf->Output('piwam.pdf', 'I');
+
+//============================================================+
+// END OF FILE
+//============================================================+
